4ec3adc7877bddb842b3fb60134f263b0fd189f5
avis3004 <sivaganeshvbe@gmail.com>
2015-04-05 22:27:24 +0530

    forget password link

--- finalmudichurpanchayat/forgotpassword.php
+++ finalmudichurpanchayat/forgotpassword.php
@@ -1,38 +1,6 @@
 <?php
 //session_start();
-include "database_connection.php"; //connects to the database
-if (isset($_POST['username'])){
-    $username = $_POST['username'];
-   $query="select * from registration where Username='$username'";
-    $result   = mysql_query($query);
-    $count=mysql_num_rows($result);
-    // If the count is equal to one, we will send message other wise display an error message.
-    if($count==1)
-    {
-	
-        $rows=mysql_fetch_array($result);
-        $pass  =  $rows['Password'];//FETCHING PASS
-        echo "your password is ::".($pass)."";
-		//$email=$rows['Email'];//Fwetching Email
-        $to = $rows['Email'];
-        echo "<br>your email is ::".$to;
-        //Details for sending E-mail
-		// $to = "sivaganeshvbe@gmail.com";
-   $subject = "This is subject";
-   $message = "This is simple text message.";
-   $header = "From:sivaganeshvbe@gmail.com \r\n";
-   
-   $retval = mail ($to,$subject,$message,$header);
-   if( $retval == true )  
-   {
-      echo "Message sent successfully...";
-   }
-   else
-   {
-      echo "Message could not be sent...";
-   }
-      }
-	  }
+include "database_connection.php";
 ?>
 <html>
 <head>
@@ -43,7 +11,48 @@ if (isset($_POST['username'])){
 <form action="" method="post">
         <label> Enter your User ID : </label>
         <input id="username" type="text" name="username" />
-        <input id="button" type="submit" name="button" value="Submit" />
+        <input id="button" type="submit" name="submit" value="Submit" />
     </form>
 </body>
 </html>
+
+<?php
+ //connects to the database
+if (isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $query="select * from registration where Username='$username'";
+    $result   = mysql_query($query);
+    $count=mysql_num_rows($result);
+    // If the count is equal to one, we will send message other wise display an error message.
+    if($count==1)
+    {
+	
+        $rows=mysql_fetch_array($result);
+        $pass  =  $rows['Password'];//FETCHING PASS
+        $txt = "your password is ::".($pass)."";
+		//$email=$rows['Email'];//Fwetching Email
+        $to = "sivaganeshvbe@gmail.com";//$rows['Email'];
+        echo "<br>your email is ::".$to;
+        //Details for sending E-mail
+		// $to = "sivaganeshvbe@gmail.com";
+        $subject = "PASSWORD RESET";
+        //$message = "his is simple text message.";
+        $header = "sivachitravijayan@gmail.com";
+   
+        //mail ($to,$subject,$txt,$header);
+       /*if( $retval == true )  
+      {
+      echo "Message sent successfully...";
+      }
+      else
+      {
+      echo "Message could not be sent...";
+      }*/
+    }
+    else
+    {
+      echo "Username does not exists in data base";
+    }
+	  }
+?>
+
